4747c40e8550772f74811f9b3fe3291e4dcbb50b
FatherCarlito <stayinghella@gmail.com>
2023-04-30 17:35:41 -0700

    delete build/dist

--- StoreFrontVersions/17B_StoreFrontV12/build/Debug/Cygwin-Windows/main.o.d
+++
@@ -1,9 +0,0 @@
-build/Debug/Cygwin-Windows/main.o: main.cpp StoreFront.h SignIn.h \
- Inventory.h ShoppingCart.h Customer.h SalesRecord.h Admin.h
-StoreFront.h:
-SignIn.h:
-Inventory.h:
-ShoppingCart.h:
-Customer.h:
-SalesRecord.h:
-Admin.h:
