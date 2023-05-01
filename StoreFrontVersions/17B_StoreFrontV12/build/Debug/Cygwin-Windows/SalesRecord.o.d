d1b658168f70e88295a9d2eb540d3fe887cb61e3
FatherCarlito <stayinghella@gmail.com>
2023-04-30 17:34:50 -0700

    added another customer purchase

---
+++ StoreFrontVersions/17B_StoreFrontV12/build/Debug/Cygwin-Windows/SalesRecord.o.d
@@ -0,0 +1,4 @@
+build/Debug/Cygwin-Windows/SalesRecord.o: SalesRecord.cpp SalesRecord.h \
+ StoreFront.h
+SalesRecord.h:
+StoreFront.h:
---
+++ StoreFrontVersions/17B_StoreFrontV12/build/Debug/Cygwin-Windows/main.o.d
@@ -0,0 +1,9 @@
+build/Debug/Cygwin-Windows/main.o: main.cpp StoreFront.h SignIn.h \
+ Inventory.h ShoppingCart.h Customer.h SalesRecord.h Admin.h
+StoreFront.h:
+SignIn.h:
+Inventory.h:
+ShoppingCart.h:
+Customer.h:
+SalesRecord.h:
+Admin.h:
